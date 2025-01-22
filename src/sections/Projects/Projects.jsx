3b3db3b7f66d1336b519ace4bb82438b11d717a1
Ismail Cayden <ismailmaho007@gmail.com>
2025-01-22 03:00:14 +0200

    Projects section added

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,19 +1,25 @@
 import styles from "./ProjectsStyles.module.css";
-import viberr from "../../assets/viberr.png";
+import portfolio from "../../assets/portfolio.png";
+import taskGoal from "../../assets/task-goal.png";
+import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <a
-          href="https://github.com/CTHartze/profile-portfolio-website"
-          target="_blank"
-        >
-          <img className="hover" src={viberr} alt="Viberr logo" />
-          <h3>Portfolio</h3>
-          <p>Profile Portfolio Website</p>
-        </a>
+        <ProjectCard
+          src={portfolio}
+          link="https://github.com/CTHartze/profile-portfolio-website"
+          h3="Portfolio"
+          p="Profile Portfolio Website"
+        />
+        <ProjectCard
+          src={taskGoal}
+          link="https://github.com/CTHartze/task-goal"
+          h3="Task-Goal"
+          p="Todo List App"
+        />
       </div>
     </section>
   );
